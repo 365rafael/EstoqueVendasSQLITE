591e977825c633f3c9301b7a1a241893552a0597
Rafael Arantes <rafael-fullstack@outlook.com>
2024-11-13 20:03:50 -0300

    correção de bug

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using EstoqueVendasSQLITE.Configurations;
 using EstoqueVendasSQLITE.Context;
 using EstoqueVendasSQLITE.Services;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,6 +29,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
 });
 
+//builder.WebHost.ConfigureKestrel(options =>
+//{
+//    options.ListenLocalhost(5000);
+//});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +44,18 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Abrir o navegador automaticamente
+//if (OperatingSystem.IsWindows())
+//{
+//    Process.Start(new ProcessStartInfo
+//    {
+//        FileName = "cmd",
+//        Arguments = "/c start http://localhost:5000",
+//        CreateNoWindow = true
+//    });
+//}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
